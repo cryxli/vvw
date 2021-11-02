6b90dbde66927f2398dbf3c964de98916de86197
Urs P. Stettler <git@cryx.li>
2021-11-02 12:35:05 +0100

    Changed from triggering logic with hash (ankers) to search (query parameters).

--- js/custom-tags.js
+++ js/custom-tags.js
@@ -3,13 +3,19 @@ window.customTags = window.customTags||function() {
 	$('npc').each((index,elem) => {
 		const text = $(elem).text();
 		const id = text.toLowerCase().replace(/ /g,'_');
-		$(elem).replaceWith('<a href="npc.htm#'+id+'">'+text+'</a>');
+		$(elem).replaceWith('<a href="npc.htm?'+id+'">'+text+'</a>');
 	});
 	
 	$('location').each((index,elem) => {
 		const text = $(elem).text();
 		const id = text.toLowerCase().replace(/ /g,'_');
-		$(elem).replaceWith('<a href="location.htm#'+id+'">'+text+'</a>');
+		$(elem).replaceWith('<a href="location.htm?'+id+'">'+text+'</a>');
+	});
+	
+	$('effect').each((index,elem) => {
+		const text = $(elem).text();
+		const id = text.toLowerCase().replace(/ /g,'_');
+		$(elem).replaceWith('<a href="effect.htm?'+id+'">'+text+'</a>');
 	});
 
 };
