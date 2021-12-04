c64fa91b6f1a7da77dd33f556cc05ce37c044375
Urs P. Stettler <git@cryx.li>
2021-12-04 13:08:08 +0100

    More NPCs

--- js/npcs.js
+++ js/npcs.js
@@ -1,14 +1,20 @@
 const npcs = [
-//	{name:'Adrian',url:'adrian.htm'},
+	{name:'Adrian',url:'adrian.htm'},
+//	{name:'Apocalypse',url:'apocalypse.htm'},
+//	{name:'Screeching Calamity',url:'calamity.htm'},
+//	{name:'Chromatic Essence',url:'chromatic.htm'},
 	{name:'Tzar Borimir',url:'borimir.htm'},
 	{name:'Patriarch Casimir',url:'casimir.htm'},
 	{name:'Damian',url:'damian.htm'},
-//	{name:'Gabriel',url:'gabriel.htm'},
+	{name:'Gabriel',url:'gabriel.htm'},
 	{name:'GebaN',url:'geban.htm'},
 	{name:'Irene',url:'irene.htm'},
 	{name:'Sir Krum',url:'krum.htm'},
 	{name:'Queen Katarina',url:'katarina.htm'},
+//	{name:'Mordra',url:'mordra.htm'},
+//	{name:'Rachnoss',url:'rachnoss.htm'},
+//	{name:'The Solicitor',url:'solicitor.htm'},
 	{name:'Private Stash',url:'stash.htm'},
-	// The Solicitor
+//	{name:'Vail',url:'vail.htm'},
 	// Victor Vran
 ];
